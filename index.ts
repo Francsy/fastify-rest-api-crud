0eb15aa180ac8704fe7ee5951d7e83b563ce609a
Francsy <francsyludens@gmail.com>
2024-02-04 23:42:06 +0000

    install/nodemon and ts-node, config scripts and create trial get

--- index.ts
+++ index.ts
@@ -2,4 +2,21 @@ import fastify from 'fastify';
 
 const server = fastify({ logger: true });
 
-server.listen({ port: 3500 });
+// GET                  C-REATE
+
+// POST                 R-EAD
+
+// PUT                  U-UPDATE
+
+// DELETE               D-ELETE
+
+server.get('/', async (req, res) => {
+    return { message: 'Hello World' };
+});
+
+try {
+    server.listen({ port: 3500 });
+    console.log('Hello World');
+} catch (error) {
+    console.log(error);
+}
